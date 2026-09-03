1068f44ea236d67c1680fcf93bf758ca439f4a8c
Bulat Vagapov <vagapov.bulat.t@yandex.ru>
2026-09-03 18:49:50 +0300

    Update auth_test.py

--- UI/auth_test.py
+++ UI/auth_test.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import expect
-from tests.pages.catalog_page import CatalogPage
-from tests.steps.login_steps import LoginSteps
-from tests.steps.catalog_steps import CatalogSteps
+from pages.catalog_page import CatalogPage
+from steps.login_steps import LoginSteps
+from steps.catalog_steps import CatalogSteps
 
 class TestLogin():
     def test_auth_login_standart_user(self, page):
